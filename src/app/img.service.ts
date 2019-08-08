dca6ae13610ca525be19d3ae61da16f0ed7f4b77
Jesus Rivera <r.jesus9107@gmail.com>
2019-08-08 09:00:54 -0700

    get data

--- src/app/img.service.ts
+++ src/app/img.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ImgService {
-
-  constructor(private _http: HttpClient) { }
+  apiUrl = "assets/api.json";
+  constructor(private _http: HttpClient) {}
 
   getImage() {
-    return this._http.get('https://jesusriv.github.io/sw_character_imgs/api.json');
+    return this._http.get(this.apiUrl);
   }
 
-   getApi(url) {
+  getApi(url) {
     return this._http.get(url);
   }
 }
